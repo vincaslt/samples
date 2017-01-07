32eb2c40c7ff3dc1b6de3beb45a24d3249af8710
Vincas Stonys <vincas.stonys@gmail.com>
2017-01-07 13:31:22 +0200

    Fix update request saga

--- ExchangeRateClient/app/sagas.js
+++ ExchangeRateClient/app/sagas.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
 import config from './config.json'
 import { REQUEST_UPDATE_RATES, REQUEST_START_UPDATES, receiveConfiguration, receiveRates, requestUpdateRates } from './actions'
-import { take, call, put, delay } from 'redux-saga/effects'
+import { take, call, put, takeLatest } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
+import qs from 'qs'
 
-function* startUpdates() {
-  yield take(REQUEST_UPDATE_RATES)
+
+function* updateRates() {
   const rates = yield call(axios.get, config.ratesEndpoint, {
     params: {
       currencyPairIds: ['70c6744c-cba2-5f4c-8a06-0dac0c4e43a1', 'a2bda952-4369-5d41-9d0b-e6c9947e5b82']
-    } // TODO: use reselect for selector and handle errors
+      // TODO: use reselect for selector and handle errors
+    },
+    paramsSerializer: (params) => qs.stringify(params, { indices: false }) 
   })
   yield put(receiveRates(rates))
   yield delay(config.interval)
@@ -22,10 +26,15 @@ export function* requestStartUpdates() {
   yield put(requestUpdateRates())
 }
 
+export function* watchStartUpdates() {
+  yield takeLatest(REQUEST_UPDATE_RATES, updateRates)
+}
+
+
 // Register sagas for running
 const sagas = [
   requestStartUpdates,
-  startUpdates
+  watchStartUpdates
 ]
 
 export function runSagas(sagaMiddleware) {
